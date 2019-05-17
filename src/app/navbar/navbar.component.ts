168edf302effd48f3757ad26654c106be4d4eca8
Simon Aliotti <sim.aliotti@gmail.com>
2019-05-17 10:23:16 +0200

    Change css on signin logout functions

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -8,9 +9,16 @@ import { AuthService } from '../services/auth.service';
 })
 export class NavbarComponent implements OnInit {
 
+  authStatus: boolean;
+  authSubscription: Subscription;
   constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authSubscription = this.authService.authSubject.subscribe(
+      (data) => {
+        this.authStatus = data;
+      }
+    );
   }
   logout(){
   this.authService.logout();
